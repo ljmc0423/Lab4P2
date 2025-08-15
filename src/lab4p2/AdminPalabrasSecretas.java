7bf9c9b19f8ab50afe364ad51f92add95b736765
Thomas-lab395 <thomas_portillo@unitec.edu>
2025-08-15 10:33:33 -0600

    JuegoAhorcadoAzar y Admin Implementado

--- src/lab4p2/AdminPalabrasSecretas.java
+++ src/lab4p2/AdminPalabrasSecretas.java
@@ -4,10 +4,41 @@
  */
 package lab4p2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author ljmc2
  */
+
 public class AdminPalabrasSecretas {
-    
+    private ArrayList<String> palabras;
+
+    public AdminPalabrasSecretas() {
+        palabras = new ArrayList<>(Arrays.asList(
+                "PECHUGA", "LOMO", "PAVO", "QUESO", "FRIJOLES",
+                "SARDINAS", "LECHE", "HUEVOS", "POLVO", "CHIA"
+        ));
+    }
+
+    public boolean agregarPalabra(String palabra) {
+        palabra = palabra.toUpperCase();
+        if (!palabras.contains(palabra)) {
+            palabras.add(palabra);
+            return true;
+        }
+        return false; // palabra duplicada
+    }
+
+    public String obtenerPalabraAzar() {
+        if (palabras.isEmpty()) return null;
+        Random rnd = new Random();
+        return palabras.get(rnd.nextInt(palabras.size()));
+    }
+
+    public ArrayList<String> getPalabras() {
+        return palabras;
+    }
 }
--- src/lab4p2/JuegoAhorcadoAzar.java
+++ src/lab4p2/JuegoAhorcadoAzar.java
@@ -4,10 +4,80 @@
  */
 package lab4p2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mayra Bardales
  */
-public class JuegoAhorcadoAzar {
-    
+
+public class JuegoAhorcadoAzar extends JuegoAhorcadoBase {
+    private AdminPalabrasSecretas admin;
+
+    public JuegoAhorcadoAzar(AdminPalabrasSecretas admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public void inicializarPalabraSecreta() {
+        palabraSecreta = admin.obtenerPalabraAzar();
+        if (palabraSecreta == null) palabraSecreta = "VACIO";
+        palabraActual = "_".repeat(palabraSecreta.length());
+    }
+
+    @Override
+    public void jugar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Juego Ahorcado (Palabra Aleatoria) ===");
+        inicializarPalabraSecreta();
+
+        while (!juegoTerminado()) {
+            mostrarFigura();
+            mostrarEstado();
+            System.out.print("Ingrese una letra: ");
+            char letra = sc.next().toUpperCase().charAt(0);
+
+            if (letrasUsadas.contains(letra)) {
+                System.out.println("Letra repetida. Intente otra.");
+                continue;
+            }
+
+            letrasUsadas.add(letra);
+            if (!verificarLetra(letra)) {
+                intentos++;
+            }
+        }
+
+        mostrarFigura();
+        if (hasGanado()) {
+            System.out.println("¡Felicidades! Ganaste. La palabra era: " + palabraSecreta);
+        } else {
+            System.out.println("¡Perdiste! La palabra era: " + palabraSecreta);
+        }
+    }
+
+    @Override
+    public void actualizarPalabraActual(char letra) {
+        StringBuilder nueva = new StringBuilder(palabraActual);
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            if (palabraSecreta.charAt(i) == letra) {
+                nueva.setCharAt(i, letra);
+            }
+        }
+        palabraActual = nueva.toString();
+    }
+
+    @Override
+    public boolean verificarLetra(char letra) {
+        if (palabraSecreta.contains(String.valueOf(letra))) {
+            actualizarPalabraActual(letra);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasGanado() {
+        return palabraActual.equals(palabraSecreta);
+    }
 }
