5d4db0467b3be6f0f9d9c1389b130a4c6566a704
ljmc <ljmc2304@gmail.com>
2025-08-15 08:38:00 -0600

    interfaz base añadida

---
+++ src/lab4p2/JuegoAhorcado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2;
+
+/**
+ *
+ * @author ljmc2
+ */
+public interface JuegoAhorcado {
+    void inicializarPalabraSecreta();
+    void jugar();
+}
